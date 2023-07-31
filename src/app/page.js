264464b359d48bb1f25cd925772d1635a921ceea
TheGodFathers <yusup.kejarid@gmail.com>
2023-07-31 15:55:46 +0700

    Build With❤️

--- src/app/page.js
+++ src/app/page.js
@@ -12,7 +12,7 @@ export default function Home() {
           </div>
           <h2 className="text-white text-xl font-semibold font-varela mt-6">Perjalanan Islami Murni Anda</h2>
           <div className="flex justify-center">
-            <p className="text-white leading-relaxed font-varela text-sm mt-2 w-2/4">
+            <p className="text-white leading-relaxed font-varela text-sm mt-2 lg:w-2/4">
               real muslim app merupakan sahabat setia dalam perjalanan spiritual Anda. Jadikan Real Muslim App sebagai teman
               setia Anda dalam menguatkan ikatan Anda dengan agama, memperdalam pengetahuan Islami, dan menjadikan ibadah
               sebagai bagian integral dari kehidupan Anda sehari-hari.
